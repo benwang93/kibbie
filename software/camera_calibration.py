3f728a811f69cd184bd53886f6659d3b839371dc
Ben Wang <benwang@umich.edu>
2023-01-05 22:08:17 -0800

    Working camera_calibration script to report polygon coordinates with mouse click

--- software/camera_calibration.py
+++ software/camera_calibration.py
@@ -23,29 +23,43 @@ scale = 0.5
 class camera_calibration:
     # camera: string filepath or int representing video capture device index
     def __init__(self, camera) -> None:
-        self.masks = [[]]
+        self.mask = []
         self.camera = camera
         self.img = None
+    
+    # Presents image and overlays any masks
+    def refresh_image(self):
+        if self.img is None:
+            return
+        
+        # Draw image
+        curr_frame = self.img.copy()
+
+        if len(self.mask) > 0:
+            # Draw polygon masks
+            # Polygon corner points coordinates
+            pts = np.array(self.mask, np.int32)
+            pts = pts.reshape((-1, 1, 2))
+        
+            curr_frame = cv2.polylines(img=curr_frame, pts=[pts], 
+                                isClosed=True, color=(255, 255, 255), thickness=2)
+        
+        cv2.imshow("img", curr_frame)
+        
 
     # function to display the coordinates of
     # of the points clicked on the image 
     def click_event(self, event, x, y, flags, params):
         # checking for left mouse clicks
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.masks[0].append(x,y)
+            self.mask.append([x,y])
+            print(f"Registered click at ({x}, {y}). masks is now: {self.mask}")
             self.refresh_image()
-    
-    def refresh_image(self):
-        if self.img is None:
-            return
-        
-        # Draw image
-        
 
 
     def main(self):
         # define a video capture object
-        vid = cv2.VideoCapture(self.camera_path)
+        vid = cv2.VideoCapture(self.camera)
 
         # Get only the first video frame
         ret, frame = vid.read()
@@ -56,45 +70,35 @@ class camera_calibration:
 
         # Downsample for faster processing
         self.img = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+        
+        # Draw initial image
+        self.refresh_image()
 
         # setting mouse handler for the image
         # and calling the click_event() function
-        cv2.setMouseCallback('img', click_event)
-        
-        while(True):
-            curr_frame = img.copy()
+        cv2.setMouseCallback('img', self.click_event)
 
-            for mask in self.masks:
-                # Draw polygon masks
-                # Polygon corner points coordinates
-                pts = np.array(mask, np.int32)
-                # pts = np.array([[25, 70], [25, 160], 
-                #                 [110, 200], [200, 160], 
-                #                 [200, 70], [110, 20]],
-                #             np.int32)
-
-                pts = pts.reshape((-1, 1, 2))
-            
-                curr_frame = cv2.polylines(img2=curr_frame, pts=[pts], 
-                                    isClosed=True, color=(255, 255, 255), thickness=2)
-            
-            cv2.imshow("img", img)
-            
-            # the 'q' button is set as the
-            # quitting button you may use any
-            # desired button of your choice
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        # Quit on key press
+        cv2.waitKey(0)
         
         # After the loop release the cap object
         vid.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
 
+        # Print final polygon
+        print("\n***************************")
+        print(f"Final mask polygon (scaled at {scale}): {self.mask}")
+        print(f"Final mask polygon unscaled: {[[x[0] / scale, x[1] / scale] for x in self.mask]}")
+        height_px = self.img.shape[0]
+        width_px = self.img.shape[1]
+        print(f"Final mask polygon normalized 0 to 1: {[[x[0] / scale / width_px, x[1] / scale / height_px] for x in self.mask]}")
+        print("***************************")
+
 
 ########################
 # Main
 ########################
 if __name__=="__main__":
-    cal = camera_calibration()
-    cal.main(camera="software/images/white_background_low_light_both_cats.mp4")
+    cal = camera_calibration(camera="software/images/white_background_low_light_both_cats.mp4")
+    cal.main()
