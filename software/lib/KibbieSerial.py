ec277290840d6c116ab2c2420158dfa11593502a
Ben Wang <benwang@umich.edu>
2023-02-18 12:29:38 -0800

    Add current sample decoding to Pi

--- software/lib/KibbieSerial.py
+++ software/lib/KibbieSerial.py
@@ -11,6 +11,8 @@ Managees reading and writing operations of Kibbie, including:
 import serial
 
 class KibbieSerial:
+    # Separator used between tokens in a message
+    SEPARATOR = ","
 
     def __init__(self):
         # Define serial port
@@ -25,6 +27,9 @@ class KibbieSerial:
         # Buffer to store received data in
         self.buffer_string = ""
 
+        # Store most recent current sample
+        self.channel_current = []
+
         # Open port
         try: 
             if(self.ser.isOpen() == False):
@@ -33,6 +38,44 @@ class KibbieSerial:
             print("error open serial port: ", e)
             raise e
     
+
+    # Getter to retrieve the last receieved current
+    # from Kibbie monitor. Returns current in amps
+    def channel_current(self, channel):
+        if channel < len(self.channel_current):
+            return self.channel_current[channel]
+        else:
+            return 0
+    
+    def set_current(self, channel, current):
+        if channel < len(self.channel_current):
+            self.channel_current[channel] = current
+        elif channel == len(self.channel_current):
+            self.channel_current.append(current)
+        else:
+            raise Exception(f"Current channel {channel} out of bounds!")
+    
+    # Helper method to process a single line of serial
+    def process_line(self, line):
+        # Remove trailing characters
+        line = line.strip("\r")
+
+        # Tokenize string for processing
+        tokens = line.split(KibbieSerial.SEPARATOR)
+        opcode = tokens[0]
+
+        if opcode == "I":
+            # Current measurement
+            for i,sample in enumerate(tokens[2:]):
+                self.set_current(i, float(sample))
+            
+            print(f"Updated current: {self.channel_current}")
+        else:
+            print(f'Unrecognized token for "{line}"')
+
+
+    # Periodic function to perform receive and send operations
+    # Call this from main run loop
     def update(self):
         if self.ser.isOpen():
             # Read/process any buffered messages
@@ -43,7 +86,8 @@ class KibbieSerial:
 
                 for i,line in enumerate(lines[:-1]):
                     # Process each line
-                    print(i, ": ", line)
+                    # print(i, ": ", line)
+                    self.process_line(line)        
                 
                 # Remove processed lines
                 self.buffer_string = lines[-1]
