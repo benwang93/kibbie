c1e5ad6045f10708be552e99430381211d18d083
Ben Wang <benwang@umich.edu>
2023-02-18 21:41:22 -0800

    Add efuse calculation to arduino sketch

--- software/monitor/current_sense_demo/current_sense_demo.ino
+++ software/monitor/current_sense_demo/current_sense_demo.ino
@@ -5,19 +5,27 @@
 
     <opcode>,<timestamp>,<value1>,<value2>,...
   
-  Supported opcodes (RX):
-  - "I": Current measurement
-  
   Supported opcodes (TX):
+  - "I": Current measurement
+  - "F": EFuse status (true = open/error, false = closed/OK)
+  
+  Supported opcodes (RX):
   
   Current measurements (exponentially filtered):
 
     I,<timestamp>,<ch0 current (A)>,<ch1 current (A)>
   
+  Efuse open status
+
+    F,<timestamp>,<ch0 amp-seconds>,<ch0 open>,<ch1 amp-seconds>,<ch1 open>
+
 */
 
 // Set to true to turn on current debug message (raw sample, unfiltered voltage, unfiltered current)
 const bool ENABLE_CURRENT_DEBUG = false;
+
+// Set to true to turn on efuse debug message (current integral)
+const bool ENABLE_EFUSE_DEBUG = true;
 
 const float FILT_LEARNING_FACTOR = 0.95;       // Each new sample is 0.9*old + (1 - 0.9) * new
 
@@ -32,21 +40,45 @@ uint8_t CURRENT_CHANNEL_PINS[] = {
 };
 
 // Array to store per-channel filtered current
-int rawValues[] = {0, 0};
-float unfilteredVoltage[] = {0, 0};
-float unfilteredCurrent[] = {0, 0};
-float filteredCurrent[] = {0, 0};
+int rawValues[NUM_CURRENT_CHANNELS] = {0, 0};
+float unfilteredVoltage[NUM_CURRENT_CHANNELS] = {0, 0};
+float unfilteredCurrent[NUM_CURRENT_CHANNELS] = {0, 0};
+float filteredCurrent[NUM_CURRENT_CHANNELS] = {0, 0};
 
+// Periodic definitions
 const int SERIAL_OUTPUT_PERIOD_MS = 100; // ms, period at which to report current measurements on serial (lower bound)
-// const int SERIAL_OUTPUT_PERIOD_MS = 100; // ms, period at which to report current measurements on serial (lower bound)
+const int SAMPLE_INPUT_PERIOD_MS = 10;  // ms, period at which to run main loop (sample current)
 unsigned long nextOutputTime = 0; // Next timestamp at which to report current measurement to serial
+unsigned long nextLoopTime = 0; // Next timestamp at which to sample inputs
+
+// Efuse
+const int EFUSE_CALC_PERIOD_MS = 500;
+unsigned long nextEfuseCalcTime = 0;  // ms timestamp to run next efuse calculation
+const float EFUSE_AMP_SECONDS_THRESHOLD = 1.0 /*A*/ * 1 /*s*/;  // current*time threshold to blow efuse
+                                                                // Currently set low for development
+                                                                // Good value is probably 1.0A for 5s or 10s
+const int NUM_SECONDS_TO_INTEGRATE = 20; // s
+const int NUM_CURRENT_WINDOW_SAMPLES = NUM_SECONDS_TO_INTEGRATE /*s*/ * (1000 / EFUSE_CALC_PERIOD_MS) /*samples/second*/;  // Integral window
+const float EFUSE_DT = (float)EFUSE_CALC_PERIOD_MS / 1000;
+float filteredCurrentSumOverWindow[NUM_CURRENT_CHANNELS] = {0.0, 0.0};                // Raw sum of all samples in window
+float filteredCurrentIntegralOverWindow[NUM_CURRENT_CHANNELS] = {0.0, 0.0};           // Multiply sum by dt to obtain integral
+float filteredCurrentWindowSamples[NUM_CURRENT_CHANNELS][NUM_CURRENT_WINDOW_SAMPLES]; // Circular buffer of samples to update sum with
+unsigned int nextFilteredCurrentWindowSamples[NUM_CURRENT_CHANNELS] = {0, 0};         // Next index in circular buffer to pop
+bool efuseOpenStatus[NUM_CURRENT_CHANNELS] = {false, false};                          // true for blown, false for closed
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // Initialize current integral window to all 0s
+  for (int channel = 0; channel < NUM_CURRENT_CHANNELS; channel++) {
+    for (int i = 0; i < NUM_CURRENT_WINDOW_SAMPLES; i++) {
+      filteredCurrentWindowSamples[channel][i] = 0.0;
+    }
+  }
 
-  Serial.println("Starting current measurements...");
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+
+  // Serial.println("Starting current measurements...");
 }
 
 void sampleAndFilter(uint8_t channel) {
@@ -66,7 +98,33 @@ void sampleAndFilter(uint8_t channel) {
   filteredCurrent[channel] = FILT_LEARNING_FACTOR * filteredCurrent[channel] + (1.0 - FILT_LEARNING_FACTOR) * current;
 }
 
-void reportCurrentOnSerial() {
+void updateEfuse(uint8_t channel) {
+  // Integrate current and update sliding window
+  for (int channel = 0; channel < NUM_CURRENT_CHANNELS; channel++) {
+    // First pop the value to remove
+    unsigned int sampleToRemoveIdx = nextFilteredCurrentWindowSamples[channel];
+    float sampleToRemove = filteredCurrentWindowSamples[channel][sampleToRemoveIdx];
+
+    // Replace it with the new sample
+    float currentSample = filteredCurrent[channel];
+    filteredCurrentWindowSamples[channel][sampleToRemoveIdx] = currentSample;
+
+    // Compute new integral
+    filteredCurrentSumOverWindow[channel] += currentSample - sampleToRemove;
+    filteredCurrentIntegralOverWindow[channel] = filteredCurrentSumOverWindow[channel] * EFUSE_DT;
+
+    // Check against limit
+    if (filteredCurrentIntegralOverWindow[channel] > EFUSE_AMP_SECONDS_THRESHOLD) {
+      // Blow the fuse and latch
+      efuseOpenStatus[channel] = true;
+    }
+
+    // Increment index
+    nextFilteredCurrentWindowSamples[channel] = ((nextFilteredCurrentWindowSamples[channel] + 1) % NUM_CURRENT_WINDOW_SAMPLES);
+  }
+}
+
+void reportCurrentOnSerial(bool debug=false) {
   String output = "";
 
   String separator = ","; // Can also use a tab instead
@@ -82,7 +140,7 @@ void reportCurrentOnSerial() {
       output += separator;
     }
 
-    if (ENABLE_CURRENT_DEBUG) {
+    if (debug) {
       output +=
         String(rawValues[channel]) + separator +          // Raw integer ADC values
         String(unfilteredVoltage[channel]) + separator +  // Unfiltered float voltage
@@ -96,16 +154,70 @@ void reportCurrentOnSerial() {
   Serial.println(output);
 }
 
+void reportEfuseStatusOnSerial(bool debug=false) {
+  String output = "";
+
+  String separator = ","; // Can also use a tab instead
+  
+  // Opcode (F for Fuse status)
+  output += "F" + separator;
+
+  // Prepend csv output with timestamp for CSV output
+  output += String(millis()) + separator;
+
+  for (int channel = 0; channel < NUM_CURRENT_CHANNELS; channel++) {
+    if (channel > 0) {
+      output += separator;
+    }
+
+    if (debug) {
+      output +=
+        // String(nextFilteredCurrentWindowSamples[channel]) + separator +   // Next sample number
+        String(filteredCurrentSumOverWindow[channel]) + separator;      // Sum of moving window
+
+        // // Print out arrays
+        // Serial.print("Ch " + String(channel) + ": ");
+        // for (int i = 0; i < NUM_CURRENT_WINDOW_SAMPLES; i++) {
+        //   Serial.print(String(filteredCurrentWindowSamples[channel][i]) + ",");
+        // }
+        // Serial.println();
+    }
+
+    output +=
+      String(filteredCurrentIntegralOverWindow[channel]) + separator +    // Integral value (A*s)
+      String(efuseOpenStatus[channel]);                                   // Efuse status (0 for OK, 1 for off)
+  }
+
+  Serial.println(output);
+}
+
 // the loop routine runs over and over again forever:
 void loop() {
+  // Sample and filter current
   for (int channel = 0; channel < NUM_CURRENT_CHANNELS; channel++) {
     sampleAndFilter(channel);
   }
 
-  // Report filtered current on serial periodically
+  // Periodically update efuse status
   unsigned long currentTime = millis();
+  if (currentTime >= nextEfuseCalcTime) {
+    for (int channel = 0; channel < NUM_CURRENT_CHANNELS; channel++) {
+      updateEfuse(channel);
+    }
+    nextEfuseCalcTime = currentTime + EFUSE_CALC_PERIOD_MS;
+  }
+
+  // Report filtered current and status on serial periodically
+  currentTime = millis();
   if (currentTime >= nextOutputTime) {
-    reportCurrentOnSerial();
+    if (ENABLE_CURRENT_DEBUG) {
+      reportCurrentOnSerial(true);
+    } else if (ENABLE_EFUSE_DEBUG) {
+      reportEfuseStatusOnSerial(true);
+    } else {
+      reportCurrentOnSerial();
+      reportEfuseStatusOnSerial();
+    }
 
     nextOutputTime = currentTime + SERIAL_OUTPUT_PERIOD_MS;
   }
