4ab7902e8764107d85767649ea9d1fbd685f6366
Ben Wang <benwang@umich.edu>
2023-02-25 18:34:21 -0800

    WIP adding web server process to kibbie

--- software/kibbie.py
+++ software/kibbie.py
@@ -13,6 +13,14 @@ from lib.KibbieSerial import KibbieSerial
 
 from lib.Parameters import *
 
+# Imports for web server
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+
 ########################
 # Constants
 ########################
@@ -695,14 +703,102 @@ def servo_process( command_queue, log_queue):
 
 
 ########################
+# Web server process
+########################
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+# initialize the video stream and allow the camera sensor to
+# warmup
+#vs = VideoStream(usePiCamera=1).start()
+# vs = VideoStream(src=0).start()
+# time.sleep(2.0)
+
+@app.route("/")
+def index():
+    # return the rendered template
+    return render_template("index.html")
+
+def generate():
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+        # yield the output frame in the byte format
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+            bytearray(encodedImage) + b'\r\n')
+
+outputFrame = None
+
+def detect_motion():
+    global outputFrame
+    # loop over frames from the video stream
+    while True:
+        # read the next frame from the video stream, resize it,
+        # convert the frame to grayscale, and blur it
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        with lock:
+            outputFrame = cv2.putText(frame, timestamp.strftime(
+                "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+@app.route("/video_feed")
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+########################
 # Main
 ########################
 if __name__=="__main__":
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=True,
+        help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+        help="ephemeral port number of the server (1024 to 65535)")
+    ap.add_argument("-f", "--frame-count", type=int, default=32,
+        help="# of frames used to construct the background model")
+    args = vars(ap.parse_args())
+    # start a thread that will perform motion detection
+    t = threading.Thread(target=detect_motion)
+    t.daemon = True
+    t.start()
+    # start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True, threaded=True, use_reloader=False)
+
     # Set up servo process
     servo_command_queue = Queue()      # Kibbie -> Servo queue for commands
     servo_log_queue = Queue()  # Servo -> Kibbie queue for logs to write to disk
     servo_process_handle = Process(target=servo_process, args=(servo_command_queue, servo_log_queue,))
     servo_process_handle.start()
+
+    # Set up web server process
+
     
     kb = kibbie(
         camera=CAMERA_DEVICE,
