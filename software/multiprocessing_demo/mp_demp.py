3317a66b8e2bedfd595691d30866827e7003da0d
Ben Wang <benwang@umich.edu>
2023-02-25 17:25:34 -0800

    Make the multiprocessing demo use a class

--- software/multiprocessing_demo/mp_demp.py
+++ software/multiprocessing_demo/mp_demp.py
@@ -3,30 +3,35 @@ import time
 
 from multiprocessing import Process, Queue
 
-def writer(q):
-    # Generate a random number (simulate servo commands)
-    while (1):
-        i = random.randint(0, 100)
-        q.put(i)
-        print(f"Writer: generated {i}")
+class MPClass:
+    def __init__(self):
+        q = Queue()
+        p1 = Process(target=self.writer, args=(q,))
+        p1.start()
+        p2 = Process(target=self.reader, args=(q,))
+        p2.start()
+        p1.join()
+        p2.join()
+        print("Done! (should never get here)")
 
-        time.sleep(1.0)
+    def writer(self, q):
+        # Generate a random number (simulate servo commands)
+        while (1):
+            i = random.randint(0, 100)
+            q.put(i)
+            print(f"Writer: generated {i}")
 
-def reader(q):
-    # Periodically sample random number and print to console
-    while (1):
-        print("Reader:")
-        while not q.empty():
-            i = q.get()
-            print(f" - {i}")
+            time.sleep(1.0)
 
-        time.sleep(5.0)
+    def reader(self, q):
+        # Periodically sample random number and print to console
+        while (1):
+            print("Reader:")
+            while not q.empty():
+                i = q.get()
+                print(f" - {i}")
+
+            time.sleep(5.0)
 
 if __name__ == '__main__':
-    q = Queue()
-    p1 = Process(target=writer, args=(q,))
-    p1.start()
-    p2 = Process(target=reader, args=(q,))
-    p2.start()
-    p1.join()
-    p2.join()
+    mp = MPClass()
