281e7d4a27a89beb89856640df379b3649bc5c29
Ben Wang <benwang@umich.edu>
2023-02-25 16:49:47 -0800

    multiprocessing demo now communicates via a queue between processes

--- software/multiprocessing_demo/mp_demp.py
+++ software/multiprocessing_demo/mp_demp.py
@@ -1,22 +1,32 @@
+import random
 import time
-from multiprocessing import Process
 
-def writer():
+from multiprocessing import Process, Queue
+
+def writer(q):
     # Generate a random number (simulate servo commands)
     while (1):
-        print("Writer")
+        i = random.randint(0, 100)
+        q.put(i)
+        print(f"Writer: generated {i}")
+
         time.sleep(1.0)
 
-def reader():
+def reader(q):
     # Periodically sample random number and print to console
     while (1):
-        print("Reader")
-        time.sleep(1.0)
+        print("Reader:")
+        while not q.empty():
+            i = q.get()
+            print(f" - {i}")
+
+        time.sleep(5.0)
 
 if __name__ == '__main__':
-    p1 = Process(target=writer)#, args=('bob',))
+    q = Queue()
+    p1 = Process(target=writer, args=(q,))
     p1.start()
-    p2 = Process(target=reader)#, args=('bob',))
+    p2 = Process(target=reader, args=(q,))
     p2.start()
     p1.join()
     p2.join()
